582cb4d3cb752f99f2035c359429e985b8e242ae
xvh422 <joey.carter555@gmail.com>
2023-12-12 16:10:31 +0000

    First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿StreamReader reader = new StreamReader("names.txt");
+string line = line = reader.ReadLine();
+reader.Close();
+
+string[] namesArray = line.Split(",");
+List<string> names = new List<string>(namesArray);
+
+for (int i=0; i < names.Count(); i++) {
+    names[i] = names[i].Replace("\u0022", "");
+}
+
+names.Sort();
+
+int totalScore = 0;
+
+for (int i=0; i < names.Count(); i++) {
+    int score = 0;
+    foreach (char c in names[i]) {
+        int index = char.ToUpper(c) - 64;
+        score += index;
+    }
+    score = score * (i + 1);
+    totalScore += score;
+}
+
+Console.WriteLine(totalScore);
